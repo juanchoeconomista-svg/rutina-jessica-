cf9e96dc292fd02930d136a6ad40c6c0b1916c54
juanchoeconomista-svg <juancho.economista@gmail.com>
2025-08-04 18:07:57 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('rutina-cache').then(cache => 
+    cache.addAll(['./', './index.html', './manifest.json', './style.css'])
+  ));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
